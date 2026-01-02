42ac6bb939383bed5a0a4a68f34a754764e98a3f
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2026-01-02 17:03:55 +0900

    refactor: Move type definitions to root for better IDE resolution

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export { default as ThemeToggle } from './src/components/ThemeToggle.astro';
+export { default as ThemeButton } from './src/components/ThemeButton.astro';
+export { default as ThemeInit } from './src/components/ThemeInit.astro';
